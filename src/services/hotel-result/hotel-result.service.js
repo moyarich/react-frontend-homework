437462cbe4ce3b2625a75940761ef9387ba73dd3
moyarich <moyarich@hotmail.com>
2021-05-01 10:43:36 -0500

    Change the service so that errors can be tracked by the component that calles the service

--- src/services/hotel-result/hotel-result.service.js
+++ src/services/hotel-result/hotel-result.service.js
@@ -1,12 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
 class HotelResultService {
-    get() {
-        return axios
-            .get('http://localhost:8080/rest/rates')
-            .then(response => response.data)
-            .catch(() => {})
-    }
+  get() {
+    return axios
+      .get("http://localhost:8080/rest/rates")
+      .then((response) => response.data)
+      .catch((error) => {
+        throw error;
+      });
+  }
 }
 
 const hotelResultService = new HotelResultService();
